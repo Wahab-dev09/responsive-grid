3a8b2752ea6629ab204758e9db289f693cef0e89
Wahab-dev09 <wahabzsec@gmail.com>
2024-12-17 20:46:16 -0600

    Made Responsive Grid

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -16,31 +16,31 @@ const Grid = () => {
     const gridDescription =
     breakpoint === 0
     ? height >= 800
-      ? "Grid: 1 column, 6 rows "
+      ? "1 column, 6 rows "
       : height >= 700
-      ? "Grid: 1 column, 5 rows"
-      : "Grid: 1 column, 4 rows"
+      ? "1 column, 5 rows"
+      : "1 column, 4 rows"
       : breakpoint === 1
-      ? "Grid: 2 columns, 3 rows"
+      ? "2 columns, 3 rows"
       : breakpoint === 2
-      ? "Grid: 3 columns, 2 rows"
-      : "Grid: 4 columns, 2 row";
+      ? "3 columns, 2 rows"
+      : "4 columns, 2 row";
     return (
         <>
             <h1>Responsive Grid</h1>
             <div className="grid-details">
-            <p>{gridDescription}</p>
-                <p>Screen: {screenType}</p>
+            <p><span>Grid :</span><span>{gridDescription}</span></p>
+            <p><span>Screen :</span><span>{screenType}</span></p>
             </div>
             <div className="grid">
-                <div><img src="https://placehold.co/600x400" alt="" /></div>
-                <div><img src="https://placehold.co/600x400" alt="" /></div>
-                <div><img src="https://placehold.co/600x400" alt="" /></div>
-                <div><img src="https://placehold.co/600x400" alt="" /></div>
-                <div><img src="https://placehold.co/600x400" alt="" /></div>
-                <div><img src="https://placehold.co/600x400" alt="" /></div>
-                <div><img src="https://placehold.co/600x400" alt="" /></div>
-                <div><img src="https://placehold.co/600x400" alt="" /></div>
+                <div>Box 1</div>
+                <div>Box 2</div>
+                <div>Box 3</div>
+                <div>Box 4</div>
+                <div>Box 5</div>
+                <div>Box 6</div>
+                <div>Box 7</div>
+                <div>Box 8</div>
             </div>
             </>
     )
